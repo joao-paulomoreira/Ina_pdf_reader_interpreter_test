3986f40c67ce3c1f745b548ca44df77481a1d62f
João Paulo Moreira <joao.moreira@opuspac.com.br>
2024-12-16 15:12:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ def carrega_modelo(api_key, tipo_arquivo, arquivo):
     Se o conteúdo parecer não ser relevante (ex.: "Enable JavaScript..."), avise o usuário para tentar um novo upload.
     '''.format(tipo_arquivo, documento)
     
-    modelo = 'gpt-3.5-turbo'
+    modelo = 'gpt-4o-mini-2024-07-18'
     chat = ChatOpenAI(model=modelo, api_key=api_key)
 
     # Gerar resumo
